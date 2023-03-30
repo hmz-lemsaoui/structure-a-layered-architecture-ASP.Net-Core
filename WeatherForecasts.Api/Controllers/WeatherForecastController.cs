62fa984334f83793e73dbf10963ce89f39f24adf
Hamza Lemsaoui <hamzalemsaoui@gmail.com>
2023-03-30 16:00:32 +0000

    structure a layered architecture

---
+++ WeatherForecasts.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using WeatherForecats.Domain.Interfaces;
+using WeatherForecats.Domain.Models;
+
+namespace WeatherForecasts.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly IWeatherForecastService _service;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return _service.ProcessTemperature();
+        }
+    }
+}
